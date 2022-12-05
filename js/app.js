3b2309b33bdfe78f960ed0d10240058fe4879694
shcolo <119753541+shcolo@users.noreply.github.com>
2022-12-05 08:03:25 +0000

    edits

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector("#butter"),
-    smooth: true,
-    tablet: { smooth: true },
-    smartphone: { smooth: true }
-});
 document.querySelector('.ham').addEventListener('click', function () {
     document.querySelector('.mobile-screen').classList.toggle('open');
 });
@@ -13,7 +7,3 @@ document.querySelector('.cross').addEventListener('click', function () {
 });
 
 window.addEventListener('scroll', function () { var nav = document.querySelector('nav'); nav.classList.toggle('sticky', window.scrollY > 0); })
-
-TweenMax.from('nav', 1, { delay: 1.5, x: -100, opacity: 0, ease: Power2.easeInOut })
-TweenMax.from('.land img', 1, { delay: 0.5, y: -100, opacity: 0, ease: Power2.easeInOut })
-TweenMax.from('.land .text', 1, { delay: 0.5, y: +100, opacity: 0, ease: Power2.easeInOut })
